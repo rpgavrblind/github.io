4703e664f0d9e277136a89ff97ca129c35351b80
rpgavrblind <144194328+rpgavrblind@users.noreply.github.com>
2023-09-05 23:54:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     const rollButton = document.getElementById("rollButton");
     const resultElement = document.getElementById("result");
-    const audioElement = document.getElementById("audioElement");
-
-    let sound = new Howl({
-        src: ["dice_roll.mp3"]
-    });
-
-    const audioDescriptions = [
-        "Um",
-        "Dois",
-        "Três",
-        "Quatro",
-        "Cinco",
-        "Seis"
-    ];
 
     rollButton.addEventListener("click", function () {
         const rollResult = Math.floor(Math.random() * 6) + 1;
         const resultDescription = `Você rolou um dado de 6 lados e obteve o resultado ${rollResult}`;
-        resultElement.textContent = `Resultado: ${rollResult}`;
-        
-        // Reproduz o som usando Howler.js
-        sound.play();
 
         // Fale o resultado usando a função de síntese de fala do navegador (Web Speech API)
         if ('speechSynthesis' in window) {
-            const speech = new SpeechSynthesisUtterance(resultDescription + ". " + audioDescriptions[rollResult - 1]);
+            const speech = new SpeechSynthesisUtterance(resultDescription);
             speech.lang = 'pt-BR'; // Defina o idioma para português (Brasil)
+
+            // Converte o número em palavras usando a API Web Speech Synthesis
+            const numbersInWords = ["Um", "Dois", "Três", "Quatro", "Cinco", "Seis"];
+            const numberInWords = numbersInWords[rollResult - 1];
+
+            // Adiciona a descrição em áudio ao texto falado
+            speech.text += `. O resultado é ${numberInWords}.`;
+
             window.speechSynthesis.speak(speech);
+
+            // Exibe o resultado na página
+            resultElement.textContent = `Resultado: ${rollResult}`;
         }
     });
 });
