5feb94de5290f32ea80017e6f0b25b0c3904638c
rpgavrblind <144194328+rpgavrblind@users.noreply.github.com>
2023-09-05 23:50:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
         src: ["dice_roll.mp3"]
     });
 
+    const audioDescriptions = [
+        "Um",
+        "Dois",
+        "Três",
+        "Quatro",
+        "Cinco",
+        "Seis"
+    ];
+
     rollButton.addEventListener("click", function () {
         const rollResult = Math.floor(Math.random() * 6) + 1;
         const resultDescription = `Você rolou um dado de 6 lados e obteve o resultado ${rollResult}`;
@@ -17,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         // Fale o resultado usando a função de síntese de fala do navegador (Web Speech API)
         if ('speechSynthesis' in window) {
-            const speech = new SpeechSynthesisUtterance(resultDescription);
+            const speech = new SpeechSynthesisUtterance(resultDescription + ". " + audioDescriptions[rollResult - 1]);
             speech.lang = 'pt-BR'; // Defina o idioma para português (Brasil)
             window.speechSynthesis.speak(speech);
         }
